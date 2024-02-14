278bad942b0fef762b63779d8d398d50bb9d8dcb
Nancyhun <nancyoforiaddo17@gmail.com>
2024-02-14 08:13:44 -0800

    task 1 & 2 commit

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,6 @@
 -- creates mysql user and grants all privileges
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- set pwd for user
-SET PASSWORD FOR 'user_0d_1'@'localhost'='user_0d_1_pwd';
-
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
 
--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,10 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- create user and set pwd for user
-CREATE USER IF NOT EXISTS 'user_0d_2';
-
--- pwd
-SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- grant only select privilege in the database to user 
 GRANT SELECT ON htbn_0d_2.* TO 'user_0d_2'@'localhost';
