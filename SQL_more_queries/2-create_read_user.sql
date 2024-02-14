e720b6b2482002cd3a40cb4112f541e0e66d4bb3
Nancyhun <nancyoforiaddo17@gmail.com>
2024-02-14 08:26:17 -0800

    task 2 commit

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,9 +2,9 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- create user and set pwd for user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 
 -- grant only select privilege in the database to user 
-GRANT SELECT ON htbn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON htbn_0d_2.* TO user_0d_2@localhost;
 
 
